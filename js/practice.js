0c0448044f9a4c66d3ba1c3e37451e2ed5b56b52
Mark Oribello <moribello@gmail.com>
2020-11-23 10:33:14 -0500

    feat: Add visibility toggling for variable areas

--- js/practice.js
+++ js/practice.js
@@ -38,6 +38,12 @@ let values = genWordProb(); // generate initial word problem
 
 const checkBut = document.getElementById("checkAnswer");
 const newBut = document.getElementById("newProb");
+const flHeader = document.getElementById("flHeader");
+const nozzleHeader = document.getElementById("nozzleHeader");
+
+//Hide friction loss and nozzle areas
+document.getElementById("flVariablesArea").style.display = "none";
+document.getElementById("nozzleVariablesArea").style.display = "none";
 
 // Event listener for generating new problem
 newBut.addEventListener('click', function () {
@@ -54,12 +60,36 @@ newBut.addEventListener('click', function () {
     document.getElementById("usrLength").value = "";
     document.getElementById("usrLength").style.backgroundColor = "#dddddd";
 });
+
 newBut.addEventListener("mouseenter", function( event ) {   
   event.target.style.backgroundColor = "ffb3b3";
 });
 newBut.addEventListener("mouseout", function( event ) {   
   event.target.style.backgroundColor = "#dddddd";
 });
+
+// Event listeners for headers
+flHeader.addEventListener('click', function() {
+    const flArea = document.getElementById("flVariablesArea");
+    if (flArea.style.display == "flex") {
+        flArea.style.display = "none";
+        flHeader.innerHTML = "Click to show Friction Loss variables"
+    } else {
+        flArea.style.display = "flex";
+        flHeader.innerHTML = "Click to hide row";
+    }
+})
+nozzleHeader.addEventListener('click', function() {
+    const nozzleArea = document.getElementById("nozzleVariablesArea");
+    if (nozzleArea.style.display == "flex") {
+        nozzleArea.style.display = "none";
+        nozzleHeader.innerHTML = "Click to show Nozzle variables"
+    } else {
+        nozzleArea.style.display = "flex";
+        nozzleHeader.innerHTML = "Click to hide row";
+    }
+})
+
 checkBut.addEventListener('click', function() {
     checkUsrAnswers();
 });
